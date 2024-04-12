5ac9be27c1a9013aca236fd1e2f72b675fc39a73
7beunseo <kimes0403@gmail.com>
2024-04-12 12:56:42 +0900

    fragment 학습

---
+++ MyApplication6/app/src/main/java/com/example/myapplication6/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.myapplication6.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    // FragmentStateAdapter를 상속받는 MyFragmentPagerAdapter 클래스를 생성함
+    class MyFragmentPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity) {
+        // 어떤 fragement를 다룰 것인지 변수를 선언해둠
+        val fragments: List<Fragment>
+        init {
+            fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment()) // 3개의 Fragment를 리스트로 담고 있음
+        }
+        override fun getItemCount(): Int {
+            return fragments.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragments[position]
+        }
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 해당되는 fragement를 포함만 시키면 됨 -> viewpager를 관리하는 adapter를 선언해주기만 하면 됨
+        binding.viewpager.adapter = MyFragmentPagerAdapter(this)
+    }
+}
