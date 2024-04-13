97f353bd235ed61dcb9c6ddcf28d385103b18b54
7beunseo <kimes0403@gmail.com>
2024-04-13 12:24:43 +0900

    [2023MID] 3개의 Fragment 생성 & 연결

--- 2023Mid/app/src/main/java/com/example/mid20220961/MainActivity.kt
+++ 2023Mid/app/src/main/java/com/example/mid20220961/MainActivity.kt
@@ -5,21 +5,45 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.mid20220961.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var toggle: ActionBarDrawerToggle
+
+    class MyFragmentAdapter(activity: FragmentActivity): FragmentStateAdapter(activity) {
+        val fragments: List<Fragment>
+
+        init {
+            fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment())
+        }
+
+        override fun getItemCount(): Int {
+            return fragments.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragments[position]
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_main)
 
+        // view 등록
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // toggle 연결
         toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.menu_opened, R.string.menu_closed)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toggle.syncState()
+
+        // adapter로 ViewPaper 연결
+        binding.viewpager.adapter = MyFragmentAdapter(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -27,6 +51,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // toggle을 클릭했을 경우
         if(toggle.onOptionsItemSelected(item))
             return true
 
