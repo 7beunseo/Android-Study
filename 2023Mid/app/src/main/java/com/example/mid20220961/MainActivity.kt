710e57619a93b9f43c4c36919d9e87052d306053
7beunseo <kimes0403@gmail.com>
2024-04-13 12:05:17 +0900

    [2023MID] DrawerLayout 구성 -> ActionBar Toolbar로 변경 필요

--- 2023Mid/app/src/main/java/com/example/mid20220961/MainActivity.kt
+++ 2023Mid/app/src/main/java/com/example/mid20220961/MainActivity.kt
@@ -2,15 +2,34 @@ package com.example.mid20220961
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.mid20220961.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.menu_opened, R.string.menu_closed)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+            return true
+
+        return super.onOptionsItemSelected(item)
     }
 }
