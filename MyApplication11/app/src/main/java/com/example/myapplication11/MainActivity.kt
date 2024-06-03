69859b5c73897d9f564b1886465fa5e117208d2e
7beunseo <kimes0403@gmail.com>
2024-06-03 12:25:31 +0900

    알림창 실습

---
+++ MyApplication11/app/src/main/java/com/example/myapplication11/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.myapplication11
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.example.myapplication11.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions() ) {
+            if (it.all { permission -> permission.value == true }) {
+                noti()
+            }
+            else {
+                Toast.makeText(this, "permission denied...", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.notificationButton.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ContextCompat.checkSelfPermission(this,"android.permission.POST_NOTIFICATIONS") == PackageManager.PERMISSION_GRANTED) {
+                    noti() // 알림 기능 수행
+                }
+                else {
+                    permissionLauncher.launch( arrayOf( "android.permission.POST_NOTIFICATIONS"  ) )
+                }
+            }
+            else {
+                noti()
+            }
+        } // binding.notificationButton
+
+    } // override fun onCreate
+
+    fun noti(){ // 알림창을 띄움
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val builder: NotificationCompat.Builder
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){     // 26 버전 이상
+            val channelId="one-channel"
+            val channelName="My Channel One"
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {   // 채널에 다양한 정보 설정
+                description = "My Channel One Description"
+                setShowBadge(true)  // 앱 런처 아이콘 상단에 숫자 배지를 표시할지 여부를 지정
+                val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                val audioAttributes = AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+                setSound(uri, audioAttributes) // 소리 발생하지 않게 해도 됨
+                enableVibration(true)
+            }
+            // 채널을 NotificationManager에 등록
+            manager.createNotificationChannel(channel)
+            // 채널을 이용하여 builder 생성
+            builder = NotificationCompat.Builder(this, channelId)
+        }
+        else {  // 26 버전 이하
+            builder = NotificationCompat.Builder(this)
+        }
+
+        // 알림의 기본 정보
+        builder.run {
+            setSmallIcon(R.drawable.small)
+            setWhen(System.currentTimeMillis())
+            setContentTitle("홍길동")
+            setContentText("안녕하세요.")
+            setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.big))
+        }
+
+        manager.notify(11, builder.build())
+    }
+}
